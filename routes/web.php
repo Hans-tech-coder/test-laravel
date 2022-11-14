372f03b427b98746e75d2c1f58c4da41b1263c31
Hans-tech-coder <obenahans@gmail.com>
2022-11-14 20:33:09 +0800

    implement basic validations required for caption and image

--- routes/web.php
+++ routes/web.php
@@ -21,6 +21,7 @@ Auth::routes();
 
 // Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 Route::get('/p/create', [App\Http\Controllers\PostsController::class, 'create']);
+Route::post('/p', [App\Http\Controllers\PostsController::class, 'store']);
 
 Route::get('profile/{user}', [App\Http\Controllers\ProfilesController::class, 'index'])->name('profile.show');
 
