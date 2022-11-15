789bac0e61f3b31d1aaad7e16c3e8762a059d043
Hans-tech-coder <obenahans@gmail.com>
2022-11-15 21:28:33 +0800

    install intervention image and enabled the GD Library

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Post;
 use Illuminate\Http\Request;
+use Intervention\Image\Facades\Image;
 
 class PostsController extends Controller
 {
@@ -25,7 +26,8 @@ class PostsController extends Controller
         $imagePath = (request('image')->store('uploads', 'public'));  
           
 
-        
+        $image = Image::make(public_path("storage/$imagePath"))->fit(1200,1200);
+        $image->save();
         auth()->user()->posts()->create([
             'caption' => $data['caption'],
             'image' => $imagePath
