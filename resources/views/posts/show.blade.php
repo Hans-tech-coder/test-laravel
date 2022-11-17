04e86ba172d30e21b8e7c5b33c41bbe8e87a3e4a
Hans-tech-coder <obenahans@gmail.com>
2022-11-17 19:58:37 +0800

    Add username in show.blade.php

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -5,7 +5,10 @@
   <!-- This is a test to create a route that will SHOW -->
   <div class="row">
     <div class="col-8">
-    <img src="/storage/{{$post->image}}" class="w-100">
+      <img src="/storage/{{$post->image}}" class="w-100">
+    </div>
+    <div class="col-4">
+      <h3>{{ $post->user->username }}</h3>
     </div>
   </div>
   
