164046400d7e8584bf8b4bada9e9ce6765f3f8e9
Hans-tech-coder <obenahans@gmail.com>
2022-11-17 20:58:51 +0800

    add edit.blade.php and create update function

--- app/Http/Controllers/ProfilesController.php
+++ app/Http/Controllers/ProfilesController.php
@@ -9,9 +9,28 @@ class ProfilesController extends Controller
 {
     public function index($user)
     {
-        $user = User::findOrFail($user);
+        /* $user = User::findOrFail($user);
         return view('profiles.index', [
             'user' => $user,
+        ]); */
+
+        return view('profiles.index', compact('user'));
+    }
+
+    public function edit(User $user)
+    {
+        return view('profiles.edit', compact('user'));
+    }
+
+    public function update()
+    {
+        $data  = request()->validate([
+            'title' => 'required',
+            'description' => 'required',
+            'url' => 'url',
+            'image' => '',
         ]);
+
+        //dd($data);
     }
 }
