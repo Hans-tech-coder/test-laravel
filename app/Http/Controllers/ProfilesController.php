6d14dc56d2bf8d6efed41d6f2432a7f4af500e7f
Hans-tech-coder <obenahans@gmail.com>
2022-11-21 20:39:50 +0800

    setup vue v-text directive for follow button

--- app/Http/Controllers/ProfilesController.php
+++ app/Http/Controllers/ProfilesController.php
@@ -10,12 +10,11 @@ class ProfilesController extends Controller
 {
     public function index(User $user)
     {
-        /* $user = User::findOrFail($user);
-        return view('profiles.index', [
-            'user' => $user,
-        ]); */ 
+        $follows = (auth()->user()) ? auth()->user()->following->contains($user->id) : false;
 
-        return view('profiles.index', compact('user'));
+         //dd($follows);
+
+        return view('profiles.index', compact('user', 'follows'));
     }
 
     public function edit(User $user)
--- resources/js/components/FollowButton.vue
+++ resources/js/components/FollowButton.vue
@@ -1,26 +1,39 @@
 <template>
     <div class="root">
-      <button class="btn btn-primary" style="margin-left:10px" @click="followUser">Follow</button>
+    
+      <button class="btn btn-primary" style="margin-left:10px" @click="followUser" v-text="buttonText"></button>
     </div>
 </template>
 
 <script>
     export default {
-        props: ['userId'],
+        props: ['userId' , 'follows'],
 
         mounted() {
             console.log('Component mounted.')
         },
 
+        data: function (){
+          return {
+            status: this.follows,
+          }
+        },
+
         methods: {
           followUser(){
-            alert("This is just a test for the Follow Button Component!!! If its working!!");
+            //alert("This is just a test for the Follow Button Component!!! If its working!!");
             axios.post('/follow/' + this.userId).then(response => {
               //alert(response.data);
               console.log(response.data);
             });
           }
         
+        },
+
+        computed: {
+          buttonText(){
+            return (this.status) ? 'Unfollow' : 'Follow';
+          }
         }
     }
 </script>
