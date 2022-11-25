f6049c921bd1dc9a5f57ef5f55f1e41916c70f9c
Hans-tech-coder <obenahans@gmail.com>
2022-11-25 21:14:52 +0800

    Update User, registerconroller and index.blade.php

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -23,7 +23,9 @@
                 </div>
             </div>
           <div class="col-8 offset-2">
+            <a href="/profile/{{$post->user->id}}">
             <img src="/storage/{{$post->image}}" class="w-100">
+            </a>
           </div>
         </div>
       </div>
@@ -49,41 +51,3 @@
  
 </div>
 @endsection
-
-<!-- 
- 
- <div class="row">
-    <div class="col-8">
-      <img src="/storage/{{$post->image}}" class="w-100">
-    </div>
-
-
-    <div class="col-4">
-
-
-       <div class="d-flex align-items-center">
-         <div style="padding-right: 15px;">
-            <img src="{{$post->user->profile->profileImage()}}" 
-                alt="profile image" 
-                class="w-100 rounded-circle" 
-                style="max-width: 50px">
-         </div>
-         <div>
-            <div style="font-weight: 700">
-              <a style="text-decoration:none;" href="/profile/{{ $post->user->id }}"> 
-              <span class="text-dark" style="font-weight: 700">{{ $post->user->username }}</span>
-              </a>
-              <a href="#" style="text-decoration: none; padding-left:5px;" class="pl-3">Follow</a>
-            </div>
-         </div>
-       </div>
-       <hr>
-       <p>
-          <a style="text-decoration:none;" href="/profile/{{ $post->user->id }}">
-            <span class="text-dark" style="font-weight: 700">{{ $post->user->username }}</span>
-          </a> 
-          {{ $post->caption }}
-       </p>
-    </div>
-  </div>
-   -->
